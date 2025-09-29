5037ec25e4130bbd43ae9d768583b546ac789987
parthsapariya745 <parthsapariyait7@gmail.com>
2025-09-29 22:42:01 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,98 @@
-import { useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import 'remixicon/fonts/remixicon.css'
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "remixicon/fonts/remixicon.css";
 
 const App = () => {
-  let [input, setInput] = useState("")
-  let [index, setIndex] = useState(null)
-  let [editInput, setEditInput] = useState("")
+  const [input, setInput] = useState("");
+  const [index, setIndex] = useState(null);
+  const [editInput, setEditInput] = useState("");
 
-  let dispatch = useDispatch()
-  let todos = useSelector(store => store.todos)
+  const dispatch = useDispatch();
+  const todos = useSelector((store) => store.todos);
 
-  let handleAdd = (e) => {
-    e.preventDefault()
-    if (input !== '') {
-      dispatch({ type: 'AddTodo', payload: input })
-      setInput("")
+  const handleAdd = (e) => {
+    e.preventDefault();
+    if (input !== "") {
+      dispatch({ type: "AddTodo", payload: input });
+      setInput("");
     } else {
-      alert("Please Enter your task")
+      alert("Please Enter your task");
     }
-  }
-
-  let handleClearAll = () => {
-    dispatch({ type: 'ClearTodo' })
-  }
-
-  let handleEdit = (i) => {
-    setEditInput(todos[i])
-    setIndex(i)
-  }
-
-  let handleSave = (i) => {
-    setIndex(null)
-    dispatch({ type: 'saveTodo', payload: {editValue: editInput, index: i} })
-  }
-
-  let handleDelete = (i) => {
-    dispatch({ type: 'DeleteTodo', payload: i })
-  }
+  };
+  const handleClearAll = () => {
+    dispatch({ type: "ClearTodo" });
+  };
+  const handleEdit = (i) => {
+    setEditInput(todos[i]);
+    setIndex(i);
+  };
+  const handleSave = () => {
+    dispatch({ type: "saveTodo", index: index, editValue: editInput });
+    setIndex(null);
+  };
+  const handleDelete = (i) => {
+    dispatch({ type: "DeleteTodo", payload: i });
+  };
 
   return (
     <div className="mx-auto px-6 pt-6 pb-8 bg-[#101727] rounded-[15px] shadow-md">
       <h1 className="text-2xl font-bold my-4 text-gray-100">Todo List</h1>
       <form className="flex space-x-2 mb-4">
-        <input type="text" onChange={(e) => setInput(e.target.value)} value={input} placeholder="Enter task..." className="py-2 px-3 w-[250px] border border-gray-700 rounded-md bg-gray-800 text-gray-100 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500" />
-        <button onClick={handleAdd} className="px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-md transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-pointer">Add</button>
-        <button onClick={handleClearAll} className="px-4 py-2 bg-red-600 hover:bg-red-700 text-white font-semibold rounded-md transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-red-500 cursor-pointer">Clear All</button>
+        <input
+          type="text"
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+          placeholder="Enter task..."
+          className="py-2 px-3 w-[250px] border border-gray-700 rounded-md bg-gray-800 text-gray-100 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        <button
+          onClick={handleAdd}
+          className="px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-md transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-pointer"
+        >
+          Add
+        </button>
+        <button
+          onClick={handleClearAll}
+          className="px-4 py-2 bg-red-600 hover:bg-red-700 text-white font-semibold rounded-md transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-red-500 cursor-pointer"
+        >
+          Clear All
+        </button>
       </form>
 
       <ul className="space-y-3">
         {todos.map((e, i) => {
           return (
-            <div key={i} className="flex items-center justify-between p-2.5 bg-gray-800 rounded-md">
-              {index === i ? <input className="py-2 px-3 w-[240px] border border-gray-600 rounded-md bg-gray-800 text-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500" onChange={(p) => setEditInput(p.target.value)} value={editInput} /> : <li className="flex-1 text-gray-100">{e}</li>}
+            <div
+              key={i}
+              className="flex items-center justify-between p-2.5 bg-gray-800 rounded-md"
+            >
+              {index === i ? (
+                <input
+                  type="text"
+                  onChange={(p) => setEditInput(p.target.value)}
+                  value={editInput}
+                  className="py-2 px-3 w-[240px] border border-gray-600 rounded-md bg-gray-800 text-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+              ) : (
+                <li className="flex-1 text-gray-100">{e}</li>
+              )}
               <div className="btn">
-                <button className="ml-2 py-2 px-3 cursor-pointer text-white rounded-md font-medium transition-colors duration-200 focus:outline-none focus:ring-2 bg-yellow-500 hover:bg-yellow-600 focus:ring-yellow-400" aria-label="Edit" onClick={() => index === i ? handleSave(i) : handleEdit(i)}>
-                  {index === i ? <i class="ri-save-fill text-[20px]"></i> : <i class="ri-edit-fill text-[20px]"></i>}
+                <button
+                  onClick={() => (index === i ? handleSave() : handleEdit(i))}
+                  className="ml-2 py-2 px-3 cursor-pointer text-white rounded-md font-medium transition-colors duration-200 focus:outline-none focus:ring-2 bg-yellow-500 hover:bg-yellow-600 focus:ring-yellow-400"
+                  aria-label="Edit"
+                >
+                  {index === i ? 
+                    <i class="ri-save-fill text-[20px]"></i>
+                  : 
+                    <i class="ri-edit-fill text-[20px]"></i>
+                  }
                 </button>
-                <button className="ml-2 py-2 px-3 bg-red-500 hover:bg-red-600 text-white rounded-md font-medium transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-red-400 cursor-pointer" aria-label="Remove" onClick={() => handleDelete(i)}>
+                <button
+                  onClick={() => handleDelete(i)}
+                  className="ml-2 py-2 px-3 bg-red-500 hover:bg-red-600 text-white rounded-md font-medium transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-red-400 cursor-pointer"
+                  aria-label="Remove"
+                >
                   <i class="ri-delete-bin-fill text-[20px]"></i>
                 </button>
               </div>
@@ -65,6 +101,6 @@ const App = () => {
         })}
       </ul>
     </div>
-  )
-}
-export default App
+  );
+};
+export default App;
--- src/Reducer.jsx
+++ src/Reducer.jsx
@@ -1,13 +1,13 @@
 let initialState = { todos: [] }
 
-export let reducer = (state = initialState, action) => {
+export const reducer = (state = initialState, action) => {
     switch(action.type) {
         case 'AddTodo':
             return { todos: [...state.todos, action.payload] }
         case 'ClearTodo':
             return { todos: [] }
         case 'saveTodo':
-            return { todos: state.todos.map(() => [...state.todos, action.payload]) }
+            return { todos: state.todos.map((e, i) => i === action.index ? action.editValue : e) }
         case 'DeleteTodo': 
             return { todos: state.todos.filter((_, i) => i !== action.payload) }
         default:
